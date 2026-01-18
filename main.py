6bd3a9d474dfad73aafc5626d8811aba0b055203
jinghui <934192776@qq.com>
2026-01-18 14:20:26 +0800

    update

--- main.py
+++ main.py
@@ -23,3 +23,20 @@ prediction_message = f"I predict that tonight, the temperature will reach {tonig
 print(prediction_message)
 
 print("Have a good day!")
+
+
+
+city = input("Enter the name of a city: ")
+temperature = input(f"Enter the temperature in {city}: ")
+
+if city and temperature:
+  temperature = int(temperature)
+  
+  if temperature > 20:
+    print(f"It is currently warm in {city} with a temperature of {temperature} degrees.")
+  elif temperature > 10:
+    print(f"It is currently chilly in {city} with a temperature of {temperature} degrees.")
+  else:
+    print(f"It is currently cold in {city} with a temperature of {temperature} degrees.")
+else:
+  print("Please try again and enter a city and temperature")
